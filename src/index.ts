ee5d7f7311d10b4fa9c9f1d80f314edae6861f41
Wyatt Sell <profwyattb@gmail.com>
2021-04-05 12:56:55 +0100

    CORS

--- src/index.ts
+++ src/index.ts
@@ -31,7 +31,7 @@ const setexAsync = promisify(client.setex).bind(client);
 // client.del("access_token");
 
 app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "http://localhost:3000"); // update to match the domain you will make the request from
+  res.header("Access-Control-Allow-Origin", "http://wyattsell.com");
   res.header(
     "Access-Control-Allow-Headers",
     "Origin, X-Requested-With, Content-Type, Accept"
